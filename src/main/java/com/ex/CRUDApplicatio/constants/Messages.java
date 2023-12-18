7448562bfef70de6e35d97de2f2d8dd0cc78da60
ketidervishi0 <dervishiketi@gmail.com>
2023-12-18 16:05:28 +0100

    Random change

--- src/main/java/com/ex/CRUDApplicatio/constants/Messages.java
+++ src/main/java/com/ex/CRUDApplicatio/constants/Messages.java
@@ -12,7 +12,7 @@ public class Messages {
     public static final String RECORDS_RECEIVED = "Records successfully received.";
     public static final String RECORD_CREATED = "Record successfully created.";
     public static final String RECORD_DELETED = "Record successfully deleted.";
-    public static final String RECORD_UPDATED = "Record successfully updated.";
+    public static final String RECORD_UPDATED = "Record successfully updated!";
     public static final String SUCCESS = "success";
     public static final String FAIL = "failed!";
     public static final String NOT_FOUND = "Not Found!";
